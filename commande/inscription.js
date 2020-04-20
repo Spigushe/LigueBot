fca149b0ce1448e589404e2e803a6b8c14e953d7
Spiguhse <martin.cuchet@gmail.com>
2020-04-20 10:53:18 +0200

    modif connexion (again)

--- commande/inscription.js
+++ commande/inscription.js
@@ -1,42 +1,27 @@
 exports.run = (client, message, Discord, prefix) => {
 	message.reply("On amorce le processus d'inscription.");
 	
-	// Connexion à la base de données
-	const mysql = require('mysql');
+	var sqlite3 = require('sqlite3');
+	var db = new sqlite3.Database('sqlite02.db');
 	
-	// Données de connexion à la base de données
-	var sql = mysql.createConnection({
-        'host':"mtgnantes.fr.mysql",
-        'port':3306,
-        'user':"mtgnantes_frdiscord_ligue",
-        'password':"LigueDuelCommander",
-        'database':"mtgnantes_frdiscord_ligue"
-	});
+	db.serialize(function() {
+	    db.run("create table if not exists log (date)");
+	    db.all("select date from log", function(err, rows) {
+	        if( rows.length == 0 )
+	        {
+	            message.channel.send("Première exécution !");
+	        }
+	        else
+	        {
+	            for( var i in rows )
+	            {
+	                console.log(rows[i].date);
+	            }
+	        }
+	    });
 	
-	// Connexion à la base de données
-	sql.connect(function(err) {
-	    if (err) {
-	        message.channel.send("ERROR: " + err.message);
-	        throw err;
-	    }
-	    console.log("connected.");
-	    message.channel.send("Connecté à " + client.host);
+	    var date = new Date().toLocaleString();
+	    var stmt = db.prepare("insert into log values (?)");
+	    stmt.run(date);
 	});
-	
-	message.channel.send('Suite de la commande');
-	/*
-	// Vérification de l'existence de la table des inscrits
-	let sql_table = "CREATE TABLE IF NOT EXISTS inscrits_ligue (" +//
-						"id_joueur			INTEGER		NOT NULL,"+//
-						"pseudo_discord		TEXT		NOT NULL,"+//
-						"pseudo_cockatrice	TEXT		NOT NULL,"+//
-						"id_liste_MV		INTEGER		NOT NULL,"+//
-						"hash_cockatrice	TEXT		NOT NULL,"+//
-						"id_role_discord	INTEGER		NOT NULL" +//
-					");";
-	client.query(sql_table, function (err) {
-		if (err) { message.channel.send(err); }
-		console.log('Création OK');
-	});
-	//*/
 }
