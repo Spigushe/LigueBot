aa6ceedd6daa13932e78700e38323444e63c7141
Spiguhse <martin.cuchet@gmail.com>
2020-04-20 13:01:19 +0200

    Update inscription.js

--- commande/inscription.js
+++ commande/inscription.js
@@ -4,11 +4,11 @@ exports.run = (client, message, Discord, prefix) => {
 	
 	var args = message.content.slice(prefix.length).trim().split(/ +/g);
 	var inscription = {
-		tag_auteur: message.author.tag,
-		id_auteur: message.author.id
+		'tag_auteur': message.author.tag,
+		'id_auteur': message.author.id
 	};
 	
-	message.channel.send(inscription);
+	message.channel.send(inscription['tag_auteur'] + " " + inscription['id_auteur']);
 	/*
 	// Test 
 	axios({
