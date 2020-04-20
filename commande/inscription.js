0de3136b7e69124353d7b56161d530646a47013c
Spiguhse <martin.cuchet@gmail.com>
2020-04-20 09:51:49 +0200

    Désignation d'un DB
    ajout d'un chemin pour un DB en dehors de :memory:
    ajout d'une commande de création de table

--- commande/inscription.js
+++ commande/inscription.js
@@ -4,14 +4,29 @@ exports.run = (client, message, Discord, prefix) => {
 	// Connexion à la base de données
 	const sqlite = require("sqlite3").verbose();
 	
-	// open database in memory
-	let db = new sqlite.Database(':memory:', (err) => {
+	// Ouverture de la base de données
+	let db = new sqlite.Database('./database/database.sqlite', (err) => {
 		if (err) { return console.error(err.message); }
 		console.log('Connected to the in-memory SQlite database.');
 		message.channel.send("Connecté à la base de données");
 	});
 	
-	// close the database connection
+	// Vérification de l'existence de la table des inscrits
+	let sql_table = "CREATE TABLE IF NOT EXISTS inscrits_ligue (" +//
+						"id_joueur			INTEGER		NOT NULL,"+//
+						"pseudo_discord		TEXT		NOT NULL,"+//
+						"pseudo_cockatrice	TEXT		NOT NULL,"+//
+						"id_liste_MV		INTEGER		NOT NULL,"+//
+						"hash_cockatrice	TEXT		NOT NULL,"+//
+						"id_role_discord	INTEGER		NOT NULL" +//
+					");";
+	db.run(sql_table, err => {
+		if (err) { return console.error(err.message); }
+		console.log("Successful creation of the 'Books' table");
+		message.channel.send("Commande de vérification de table inscrits_ligue OK");
+	});
+	
+	// Fermeture de la connexion
 	db.close((err) => {
 		if (err) { return console.error(err.message); }
 		console.log('Close the database connection.');
