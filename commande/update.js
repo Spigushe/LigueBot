14ad70f98da995aaf24ed076fe1c38a34f3888d8
Spiguhse <martin.cuchet@gmail.com>
2020-04-21 15:54:02 +0200

    Update update.js

--- commande/update.js
+++ commande/update.js
@@ -13,29 +13,21 @@ exports.run = (client, message, Discord, prefix) => {
 	}
 	
 	for (let i = 2; i < args.length; i++) {
-		message.reply("mtgnantes/index.php?page=Role&action=Retirer&pseudo="+args[i]);
-		message.reply("mtgnantes/index.php?page=Role&action=Attribuer&role="+args[1]+"&pseudo="+args[i]);
+		// Recherche des informations pour update
+		axios.get("http://ligue.mtgnantes.fr/index.php?page=Role&action=Attribuer&role="+args[1]+"&pseudo="+args[i])
+		.then( function (response) {
+			// La connexion à la page a réussi
+			if (response.data.match(/erreur/gi)) {
+				message.author.send("L'update de "+args[i]+" a rencontré un problème : " + response.data);
+			}
+			if (response.data.match(/--/gi)) {
+				infos = response.data.split("--");
+				message.guild.members.fetch(infos[0]).roles.remove(infos[1]).add(infos[2]);
+				message.author.send("L'ajout du nouveau rôle est fait");
+			}
+		}).catch( function (error) {
+			// La connexion à la page a échoué
+			message.channel.send('Erreur : ' + error );
+		});
 	}
-	/*
-	// Création de la chaine de données pour GET
-	let informations =  "&nom=" + args[1];
-	informations = informations + "&id_discord=" + args[2];
-	//*/
-	
-	// Ajout dans la base
-	/*
-	axios.get("http://ligue.mtgnantes.fr/index.php?page=Role&action=Ajouter"+informations)
-	.then( function (response) {
-		// La connexion à la page a réussi
-		if (response.data.match(/erreur/gi)) {
-			message.author.send("La création d'un nouveau rôle a rencontré un problème : " + response.data);
-		}
-		if (response.data.match(/ok/gi)) {
-			message.author.send("L'ajout du nouveau rôle est fait");
-		}
-	}).catch( function (error) {
-		// La connexion à la page a échoué
-		message.channel.send('Erreur : ' + error );
-	});
-	//*/
 }
