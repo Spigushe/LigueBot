66068434bf0a043a16a0b2481d696a344a663377
Spiguhse <martin.cuchet@gmail.com>
2020-04-23 18:11:19 +0200

    Update inscription.js

--- commande/inscription.js
+++ commande/inscription.js
@@ -36,8 +36,8 @@ exports.run = (client, message, Discord, prefix) => {
 		}
 		if (response.data.match(/ok/gi)) {
 			// Retour OK--id_role_placement
-			message.author.send("Ton inscription a bien été validée");
 			message.member.roles.add( response.data.split("--")[1] );
+			message.author.send("Ton inscription a bien été validée");
 			message.guild.owner.send("L'utilisateur discord : " + message.author.tag + " s'est inscrit(e) à la ligue !");
 		}
 	}).catch( function (error) {
