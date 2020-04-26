bb66b9a9a12cf5b734018caa02bae7a09884225b
Spiguhse <martin.cuchet@gmail.com>
2020-04-26 16:00:14 +0200

    Update test.js

--- commande/test.js
+++ commande/test.js
@@ -1,4 +1,18 @@
 exports.run = (client, message, Discord, prefix) => {
+	let membersWithRole = message.guild.members.filter(member => { 
+        return member.roles.find("name", "Judge");
+    }).map(member => {
+        return member.user.username;
+    })
+
+    let embed = new discord.RichEmbed({
+        "title": `Users with the ${roleName} role`,
+        "description": membersWithRole.join("\n"),
+        "color": 0xFFFF
+    });
+
+    return message.channel.send({embed});
+	
 	// Role d'arbitre = 700700174393016330
-	message.guild.members.cache.get('178851989856190464').send("test");
+	//message.guild.members.cache.get('178851989856190464').send("test");
 }
