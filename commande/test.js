b6e3f3638fbceafee7d74c85ff1aec571d851348
Spiguhse <martin.cuchet@gmail.com>
2020-04-26 15:18:05 +0200

    Update test.js

--- commande/test.js
+++ commande/test.js
@@ -1,4 +1,5 @@
 exports.run = (client, message, Discord, prefix) => {
 	// Role d'arbitre = 700700174393016330
-	message.author.reply("Liste des rôles : "+message.author.roles);
+	message.author.reply("Liste des rôles : "+message.author.roles.has('700700174393016330');
+	message.author.reply("Liste des rôles : "+message.author.roles.has('Judge');
 }
