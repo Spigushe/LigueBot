e138138de0aaae653738a0e0b311b99d70993757
Spiguhse <martin.cuchet@gmail.com>
2020-05-01 10:36:30 +0200

    Update liste.js

--- commande/liste.js
+++ commande/liste.js
@@ -18,22 +18,16 @@ exports.run = (client, message, Discord, prefix) => {
 	message.reply("mtgnantes/index.php?page=Inscription&action=Liste"+informations);
 	
 	// Ajout dans la base
-	/*
+	//*
 	axios.get("http://ligue.mtgnantes.fr/index.php?page=Inscription&action=Liste"+informations)
 	.then( function (response) {
 		// La connexion à la page a réussi
 		if (response.data.match(/erreur/gi)) {
-			message.guild.owner.send(message.author.tag+" a demandé à quitter la ligue. Cette action a rencontré un problème : "+response.data);
 			message.author.reply("Ta demande a rencontré un problème, un membre du staff viendra vers toi");
 		}
 		if (response.data.match(/--/gi)) {
-			// Retour : pseudo--role--id_role
-			message.author.send("Ta participation à la ligue a été retirée. Nous souhaitons te revoir au plus vite !");
-			message.guild.owner.send("C'est un sombre jour pour la ligue, "+//
-				message.author.tag+" est parti.\n"+//
-				"Il participait à la ligue "+response.data.split("--")[1]+".\n"+//
-				"Son pseudo cockatrice est "+response.data.split("--")[0]);
-			message.member.roles.remove(response.data.split("--")[2]);
+			// Retour : pseudo(hash) pseudo(hash) ...
+			message.author.reply("Liste des participants de la ligue " + args[1] + " : " + response.data );
 		}
 	}).catch( function (error) {
 		// La connexion à la page a échoué
