7f2e2c6bdb808f86daaab195d36faaaddf69077b
Spigushe <martin.cuchet@gmail.com>
2020-05-21 14:22:40 +0200

    i18n

--- commande/drop.js
+++ commande/drop.js
@@ -1,7 +1,9 @@
 exports.run = (client, message, Discord, prefix) => {
 	// Controle channel d'envoi
 	if (message.channel.id != '702453959436009523') {
-		message.reply("Ce n'est pas le bon channel pour cette commande");
+		message.reply("This is not the right channel to execute this command\n"+//
+					  "-------------\n"+//
+					  "Ce n'est pas le bon channel pour lancer cette commande");
 		return true;
 	}
 
@@ -24,11 +26,15 @@ exports.run = (client, message, Discord, prefix) => {
 		// La connexion à la page a réussi
 		if (response.data.match(/erreur/gi)) {
 			message.guild.owner.send(message.author.tag+" a demandé à quitter la ligue. Cette action a rencontré un problème : "+response.data);
-			message.author.send("Ta demande a rencontré un problème, un membre du staff viendra vers toi");
+			message.author.send("Your request has encountered an issue, a staff member will reach out soon\n"+//
+						  "-------------\n"+//
+						  "Ta demande a rencontré un problème, un membre du staff viendra vers toi");
 		}
 		if (response.data.match(/--/gi)) {
 			// Retour : pseudo--role--id_role
-			message.author.send("Ta participation à la ligue a été retirée. Nous souhaitons te revoir au plus vite !");
+			message.author.send("Your withdrawal has been taken into account. We wish to see you again soon\n"+//
+						  "-------------\n"+//
+						  "Ta participation à la ligue a été retirée. Nous souhaitons te revoir au plus vite !");
 			message.guild.owner.send("C'est un sombre jour pour la ligue, "+//
 				message.author.tag+" est parti.\n"+//
 				"Il participait à la ligue "+response.data.split("--")[1]+".\n"+//
@@ -37,7 +43,9 @@ exports.run = (client, message, Discord, prefix) => {
 		}
 	}).catch( function (error) {
 		// La connexion à la page a échoué
-		message.author.send("Erreur lors de l'exécution de la commande : " + error );
+		message.author.send("Error whilst executing the command :\n"+//
+					  "-------------\n"+//
+					  "Erreur lors de l'exécution de la commande : \n" + error );
 	});
 	//*/
 }
