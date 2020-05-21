b74ef3a212f2c14aff9dd7181af0562ad352f228
Spigushe <martin.cuchet@gmail.com>
2020-05-21 14:22:18 +0200

    i18n

--- commande/changer.js
+++ commande/changer.js
@@ -1,46 +1,56 @@
 exports.run = (client, message, Discord, prefix) => {
 	// Controle channel d'envoi
 	if (message.channel.id != '698829793470316545') {
-		message.reply("Ce n'est pas le bon channel pour annoncer son changement de deck");
+		message.reply("This is not the right channel to execute this command\n"+//
+					  "-------------\n"+//
+					  "Ce n'est pas le bon channel pour lancer cette commande");
 		return true;
 	}
-	
+
 	// Appel du package axios
 	const axios = require('axios').default;
-	
+
 	// Informations de la commande
 	var args = message.content.slice(prefix.length).trim().split(/ +/g);
-	
+
 	// Contrôle des données envoyées
 	// Contrôle du nombre de segments
-	if (args.length != 3) { 
-		message.author.send("il manque des informations pour effectuer l'inscription, veuillez recommencer");
+	if (args.length != 3) {
+		message.author.send("I am missing some informations to update your deck, please try again\n"+//
+					  "-------------\n"+//
+					  "Il manque des informations pour effectuer le changement, veuillez recommencer");
 		return false;
 	}
 	// Contrôle du lien Magic-Ville
 	if (!(args[2].match(/magic-ville/gi)) && !(args[2].match(/showdeck/gi))) {
-		message.author.send("le lien fourni n'est pas un lien vers un deck Magic-Ville");
+		message.author.send("The link you gave is not a Magic-Ville link\n"+//
+					  "-------------\n"+//
+					  "Le lien fourni n'est pas un lien vers un deck Magic-Ville");
 		return false;
 	}
-	
+
 	// Création de la chaine de données pour GET
 	let informations =  "&id=" + message.author.id;
 	informations = informations + "&hash=" + args[1];
 	informations = informations + "&liste=" + args[2].split("=")[1];
-	
+
 	//Test en local
 	//message.reply("mtgnantes/index.php?page=Inscription&action=Changer"+informations);
-	
+
 	// Ajout dans la base
 	axios.get("http://ligue.mtgnantes.fr/index.php?page=Inscription&action=Changer"+informations)
 	.then( function (response) {
 		// La connexion à la page a réussi
 		if (response.data.match(/erreur/gi)) {
-			message.author.send("Ton changement de deck a rencontré un problème : " + response.data);
+			message.author.send("Your deck-updating has encountered an issue :\n"+//
+						  "-------------\n"+//
+						  "Ton changement de deck a rencontré un problème : \n" + response.data);
 			//message.guild.owner.send("Une saisie a rencontré un problème : " + response.data + "\n" + message.content);
 		}
 		if (response.data.match(/ok/gi)) {
-			message.author.send("Ton changement de deck a bien été validé");
+			message.author.send("Your deck-updating has been validated\n"+//
+						  "-------------\n"+//
+						  "Ton changement de deck a bien été validé");
 			// Message pour Martin
 			message.guild.members.cache.get('178851989856190464').send("Changement de deck : " + args[2]);
 			// Message pour Guillaume
@@ -48,6 +58,8 @@ exports.run = (client, message, Discord, prefix) => {
 		}
 	}).catch( function (error) {
 		// La connexion à la page a échoué
-		message.author.send("Erreur lors de l'exécution de la commande : " + error );
+		message.author.send("Error whilst executing the command :\n"+//
+					  "-------------\n"+//
+					  "Erreur lors de l'exécution de la commande : \n" + error );
 	});
 }
