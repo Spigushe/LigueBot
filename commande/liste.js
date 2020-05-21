d8fe06231469af5b14665d4eb04ea4920afe442e
Spigushe <martin.cuchet@gmail.com>
2020-05-21 14:25:48 +0200

    i18n + fix

--- commande/liste.js
+++ commande/liste.js
@@ -1,22 +1,22 @@
 exports.run = (client, message, Discord, prefix) => {
 	// Appel du package axios
 	const axios = require('axios').default;
-	
+
 	// Informations de la commande
 	var args = message.content.slice(prefix.length).trim().split(/ +/g);
-	
+
 	// Contrôle du nombre de segments
-	if (args.length != 2) { 
-		message.author.send("il manque des informations pour effectuer l'inscription, veuillez recommencer");
+	if (args.length != 2) {
+		message.author.send("il manque des informations pour exécuter la commande");
 		return false;
 	}
-	
+
 	// Création de la chaine de données pour GET
 	let informations =  "&role=" + args[1];
-	
+
 	//Test en local
 	//message.reply("mtgnantes/index.php?page=Inscription&action=Liste"+informations);
-	
+
 	// Ajout dans la base
 	//*
 	axios.get("http://ligue.mtgnantes.fr/index.php?page=Inscription&action=Liste"+informations)
@@ -29,7 +29,9 @@ exports.run = (client, message, Discord, prefix) => {
 		message.reply("Liste des participants de la ligue " + args[1] + " : " + response.data );
 	}).catch( function (error) {
 		// La connexion à la page a échoué
-		message.author.send("Erreur lors de l'exécution de la commande : " + error );
+		message.author.send("Error whilst executing the command :\n"+//
+					  "-------------\n"+//
+					  "Erreur lors de l'exécution de la commande : \n" + error );
 	});
 	//*/
 }
