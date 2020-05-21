a7910e5645f8479174fe5093a9438486a221cd83
Spigushe <martin.cuchet@gmail.com>
2020-05-21 17:29:18 +0200

    i18n

--- commande/pause.js
+++ commande/pause.js
@@ -1,7 +1,9 @@
 exports.run = (client, message, Discord, prefix) => {
 	// Controle channel d'envoi
 	if (message.channel.id != '702453593328058389') {
-		message.reply("Ce n'est pas le bon channel pour cette commande");
+		message.reply("This is not the right channel to execute this command\n"+//
+					  "-------------\n"+//
+					  "Ce n'est pas le bon channel pour lancer cette commande");
 		return true;
 	}
 
@@ -23,19 +25,26 @@ exports.run = (client, message, Discord, prefix) => {
 	.then( function (response) {
 		// La connexion à la page a réussi
 		if (response.data.match(/erreur/gi)) {
-			message.guild.owner.send(message.author.tag+" a demandé sa mise en pause. Celle-ci a rencontré un problème : "+response.data);
-			message.author.send("Ta demande a rencontré un problème, un membre du staff viendra vers toi");
+			message.guild.owner.send(message.author.tag+" a demandé sa mise en pause. Cette action a rencontré un problème : \n"+//
+						  response.data);
+			message.author.send("Your request has encountered an issue, a staff member will reach out soon\n"+//
+						  "-------------\n"+//
+						  "Ta demande a rencontré un problème, un membre du staff viendra vers toi");
 		}
 		if (response.data.match(/--/gi)) {
 			// Retour : pseudo--role
-			message.author.send("Votre participation à la ligue a été mise en pause pour le mois en cours");
+			message.author.send("Your pause has been taken into account. We will wait for you next season\n"+//
+						  "-------------\n"+//
+						  "Votre participation à la ligue a été mise en pause pour le mois en cours");
 			message.guild.owner.send(message.author.tag+" a mis en pause sa participation.\n"+//
 				"Il participait à la ligue "+response.data.split("--")[1]+".\n"+//
 				"Son pseudo cockatrice est "+response.data.split("--")[0]);
 		}
 	}).catch( function (error) {
 		// La connexion à la page a échoué
-		message.author.send("Erreur lors de l'exécution de la commande : " + error );
+		message.author.send("Error whilst executing the command :\n"+//
+					  "-------------\n"+//
+					  "Erreur lors de l'exécution de la commande : \n" + error );
 	});
 	//*/
 }
