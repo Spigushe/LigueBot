0b544cc23ef5156de8b65f111e3383a6ab28ffa4
Spigushe <martin.cuchet@gmail.com>
2020-06-16 00:15:39 +0200

    Delete _test.js

--- commande/_test.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, Discord, prefix) => {
-	message.guild.members.filter(member => { 
-        message.channel.send('arbitre : ' + member.roles.find("id", "700700174393016330"));
-    });
-	
-	// Role d'arbitre = 700700174393016330
-	//message.guild.members.cache.get('178851989856190464').send("test");
-}
