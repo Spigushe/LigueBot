57a8cfa95ab7e314c76f22e4a0a2181c8fc3d185
Spigushe <martin.cuchet@gmail.com>
2020-06-16 15:43:20 +0000

    Update liste.js

--- commande/liste.js
+++ commande/liste.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, Discord, prefix) => {
 	// Appel du package axios
-	const axios = require('axios').default;
+	const axios = require("axios").default;
 
 	// Informations de la commande
 	var args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -14,11 +14,7 @@ exports.run = (client, message, Discord, prefix) => {
 	// Création de la chaine de données pour GET
 	let informations =  "&role=" + args[1];
 
-	//Test en local
-	//message.reply("mtgnantes/index.php?page=Inscription&action=Liste"+informations);
-
 	// Ajout dans la base
-	//*
 	axios.get("http://ligue.spigushe.com/index.php?page=Inscription&action=Liste"+informations)
 	.then( function (response) {
 		// La connexion à la page a réussi
@@ -29,9 +25,6 @@ exports.run = (client, message, Discord, prefix) => {
 		message.reply("Liste des participants de la ligue " + args[1] + " : " + response.data );
 	}).catch( function (error) {
 		// La connexion à la page a échoué
-		message.author.send("Error whilst executing the command :\n"+//
-					  "-------------\n"+//
-					  "Erreur lors de l'exécution de la commande : \n" + error );
+		message.author.send("Error whilst executing the command :\n-------------\nErreur lors de l'exécution de la commande : \n" + error );
 	});
-	//*/
-}
+};
