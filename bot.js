9d89ca88db3047b494f7ae78169a0b67acff74f9
Spigushe <martin.cuchet@gmail.com>
2020-06-16 15:29:58 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,33 @@
 // Connexion Discord
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 // Préparation de l'écoute
 var prefix = "!";
 
-client.on('ready', () => {});
+client.on("ready", () => {});
 
-client.on('message', message => {
-	if (message.author.bot) return;
+client.on("message", (message) => {
+	// On ne contrôle pas les messages d'un bot
+	if (message.author.bot) {
+		return;
+	}
+	// Est-ce que ça commence par un prefix ?
 	if (message.content.startsWith(prefix)) {
 		var args = message.content.slice(prefix.length).trim().split(/ +/g);
 		var commande = args.shift().toLowerCase();
-
-		var commandes_judgebot = ['card','price','ruling','rule','legal','hangman','standard','cr','ipg','mtr','jar','help'];
-		for (let i = 0; i < commandes_judgebot.length; i++) {
-			if (commande === commandes_judgebot[i]) {
+		
+		// Conflit avec JudgeBot
+		var judgebot = ["card","price","ruling","rule","legal","hangman","standard","cr","ipg","mtr","jar","help"];
+		for (let i = 0; i < judgebot.length; i++) {
+			if (commande === judgebot[i]) {
 				return false;
 			}
 		}
 
+		// On exécute la commande
 		try {
-			let fichierCommande = require(`./commande/${commande}.js`);
+			let fichierCommande = require("./commande/"+commande+".js");
 			fichierCommande.run(client, message, Discord, prefix);
 		} catch (err) {
 			message.author.send("Cette commande n'existe pas");
@@ -30,6 +36,5 @@ client.on('message', message => {
 	}
 });
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 //BOT_TOKEN is the Client Secret
