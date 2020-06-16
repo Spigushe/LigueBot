a8d376ec983a380c309df3c4eca6bece1e72b637
Spigushe <martin.cuchet@gmail.com>
2020-06-16 15:47:26 +0000

    Update mylist.js

--- commande/mylist.js
+++ commande/mylist.js
@@ -1,41 +1,22 @@
 exports.run = (client, message, Discord, prefix) => {
-	// Controle channel d'envoi
-	/*
-    if (message.channel.id != '702453593328058389') {
-		message.reply("Ce n'est pas le bon channel pour cette commande");
-		return true;
-	}
-    //*/
-
 	// Appel du package axios
-	const axios = require('axios').default;
-
-	// Contrôle des données envoyées
-	// Seul le message !giveup est demandé
+	const axios = require("axios").default;
 
 	// Création de la chaine de données pour GET
 	let informations =  "&id=" + message.author.id;
 
-	//Test en local
-	//message.author.send("mtgnantes/index.php?page=Inscription&action=Retrieve"+informations);
-
-	// Ajout dans la base
-	//*
+	// Récupération des infos
 	axios.get("http://ligue.spigushe.com/index.php?page=Inscription&action=Retrieve"+informations)
 	.then( function (response) {
 		// La connexion à la page a réussi
 		if (response.data.match(/erreur/gi)) {
 			message.author.send("Ta demande a rencontré un problème, nous t'invitons à réessayer");
 		} else {
-            var msg = "Voici votre liste enregistrée / You can find your current list here \n"+//
-			         response.data;
+            var msg = "Voici votre liste enregistrée / You can find your current list here \n"+response.data;
             message.author.send(msg);
 		}
 	}).catch( function (error) {
 		// La connexion à la page a échoué
-		message.author.send("Error whilst executing the command :\n"+//
-					  "-------------\n"+//
-					  "Erreur lors de l'exécution de la commande : \n" + error );
+		message.author.send("Error whilst executing the command :\n-------------\nErreur lors de l'exécution de la commande : \n" + error );
 	});
-	//*/
-}
+};
