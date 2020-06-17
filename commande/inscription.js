92c5aec7936ee775efd431090ae16d98b70ec7a4
Spigushe <martin.cuchet@gmail.com>
2020-06-17 13:07:51 +0200

    Update inscription.js

--- commande/inscription.js
+++ commande/inscription.js
@@ -1,14 +1,12 @@
 exports.run = (client, message, Discord, prefix) => {
 	// Controle channel d'envoi
-	if (message.channel.id !== '693827015610204212') {
-		message.reply("This is not the right channel to execute this command\n"+//
-					  "-------------\n"+//
-					  "Ce n'est pas le bon channel pour lancer cette commande");
+	if (message.channel.id !== "693827015610204212") {
+		message.reply("This is not the right channel to execute this command\nCe n'est pas le bon channel pour lancer cette commande");
 		return true;
 	}
 
 	// Appel du package axios
-	const axios = require('axios').default;
+	const axios = require("axios").default;
 
 	// Informations de la commande
 	var args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -16,51 +14,35 @@ exports.run = (client, message, Discord, prefix) => {
 	// Contrôle des données envoyées
 	// Contrôle du nombre de segments
 	if (args.length !== 4) {
-		message.author.send("I am missing some informations to register you, please try again\n"+//
-					  "-------------\n"+//
-					  "Il manque des informations pour effectuer le changement, veuillez recommencer");
+		message.author.send("I am missing some informations to register you, please try again\nIl manque des informations pour effectuer le changement, veuillez recommencer");
 		return false;
 	}
 	// Contrôle du lien Magic-Ville
 	if (!(args[3].match(/magic-ville/gi)) && !(args[3].match(/showdeck/gi))) {
-		message.author.send("The link you gave is not a Magic-Ville link\n"+//
-					  "-------------\n"+//
-					  "Le lien fourni n'est pas un lien vers un deck Magic-Ville");
+		message.author.send("The link you gave is not a Magic-Ville link\nLe lien fourni n'est pas un lien vers un deck Magic-Ville");
 		return false;
 	}
 
 	// Création de la chaine de données pour GET
-	let informations =  "&id=" + message.author.id;
-	informations = informations + "&pseudo=" + args[1];
-	informations = informations + "&hash=" + args[2];
-	informations = informations + "&liste=" + args[3].split("=")[1];
-
-	//Test en local
-	//message.reply("mtgnantes/index.php?page=Inscription&action=Ajouter"+informations);
+	let informations =  "&id=" + message.author.id + "&pseudo=" + args[1] + "&hash=" + args[2] + "&liste=" + args[3].split("=")[1];
 
 	// Ajout dans la base
 	axios.get("http://ligue.spigushe.com/index.php?page=Inscription&action=Ajouter"+informations)
 	.then( function (response) {
 		// La connexion à la page a réussi
 		if (response.data.match(/erreur/gi)) {
-			message.author.send("Your request has encountered an issue:\n"+//
-						  "-------------\n"+//
-						  "Ta demande a rencontré un problème :\n" + response.data);
+			message.author.send("Your request has encountered an issue:\nTa demande a rencontré un problème :\n" + response.data);
 			//message.guild.owner.send("Une saisie a rencontré un problème : " + response.data + "\n" + message.content);
 		}
 		if (response.data.match(/ok/gi)) {
 			// Retour OK--id_role_placement
 			message.member.roles.add( response.data.split("--")[1] );
-			message.author.send("Your registration has been validated\n"+//
-						  "-------------\n"+//
-						  "Ton inscription a bien été validée");
+			message.author.send("Your registration has been validated\nTon inscription a bien été validée");
 			message.guild.owner.send("L'utilisateur discord : " + message.author.tag + " s'est inscrit(e) à la ligue !");
-			message.guild.members.cache.get('178851989856190464').send("Nouvel inscrit : " + args[1] + "\nListe Magic-Ville : " + args[3]);
+			message.guild.members.cache.get("178851989856190464").send("Nouvel inscrit : " + args[1] + "\nListe Magic-Ville : " + args[3]);
 		}
 	}).catch( function (error) {
 		// La connexion à la page a échoué
-		message.author.send("Error whilst executing the command :\n"+//
-					  "-------------\n"+//
-					  "Erreur lors de l'exécution de la commande : \n" + error );
+		message.author.send("Error whilst executing the command :\nErreur lors de l'exécution de la commande : \n" + error );
 	});
-}
+};
