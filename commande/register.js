0436826bee247c845d637f6a969a062538f0cee8
Spigushe <martin.cuchet@gmail.com>
2020-12-06 23:26:18 +0100

    Update register.js

--- commande/register.js
+++ commande/register.js
@@ -1,5 +1,4 @@
 // Registration Command
-
 exports.run = (client, message, Discord, prefix) => {
 	// Channel Control
 	if (message.channel.id !== "693827015610204212") {
@@ -11,11 +10,7 @@ exports.run = (client, message, Discord, prefix) => {
 	let nick = message.content.slice(prefix.length).trim().split(/ +/g)[1];
 
 	// Notify in the proper channel
-	/*
-	let guild = client.guilds.get("693799306398007316");
-	let channel = client.channels.get("785259925143420949");
-
-	channel.send("coucou");
-	//*/
-	client.channels.cache.get("785259925143420949").send("coucou");
+	client.channels.cache
+		.get("785259925143420949")
+		.send("Inscription de <@"+message.author.id+">\nPseudo Cockatrice : "+nick);
 };
