93e48f062d9fe374edd43397875feb8d68173bee
Spigushe <martin.cuchet@gmail.com>
2020-12-07 17:13:47 +0100

    Bot Functionning
    Next step :
    - Deploy node app
    - Host on VPS

--- bot.js
+++ bot.js
@@ -9,8 +9,6 @@ const prefix = "!";
 client.on("ready", () => {});
 
 client.on("message", (message, client, Discord) => {
-	console.log(message.content);
-
 	// On ne contrôle pas les messages d'un bot
 	if (message.author.bot) { return false; }
 
@@ -21,19 +19,48 @@ client.on("message", (message, client, Discord) => {
 
 		// 📝 Inscription
 		if ((commande === "inscription") || (commande === "register")) {
-			let chemin = "./commande/register.js";
-			let fichierCommande = require(chemin);
-			fichierCommande.run(client, message, Discord, prefix);
+			// Notify in the proper channel
+			message.client.channels.cache
+				.get("785278831384723527")
+				.send("📝 **Nouvelle inscription** pour <@"+message.author.id+">\nPseudo "+args[0]);
+			// Give dedicated role
+			message.member.roles.add("774314371001352233");
+			// Delete message if not in DM
+			if (message.guild !== null) { message.delete(); }
 		}
 
 		// 🎲 Envoi de deck
 		if (commande === "deck") {
-			let chemin = "./commande/deck.js";
-			let fichierCommande = require(chemin);
-			fichierCommande.run(client, message, Discord, prefix);
+			// Notify in the proper channel
+			message.client.channels.cache
+				.get("785278831384723527")
+				.send("🎲 **Nouveau deck** pour <@"+message.author.id+">\n("+args[0]+") "+args[1]);
+			// Delete message if not in DM
+			if (message.guild !== null) { message.delete(); }
 		}
 
 		// 🎮 Saisie de résultat
+		if ((commande === "resultat") || (commande === "result")) {
+			message.author.send("You sent the following results : ```"+message.content+"```");
+			// Prepare string
+			let j1 = args[0];
+			let j2 = args[4];
+			let r1 = args[1] *1;
+			let r2 = args[3] *1;
+			if ((r1 == r2) || ((r1 + r2) > 3)) {
+				message.author.send("The results does not seem correct, please re-send them");
+				return false;
+			}
+			if (r1 < r2) {
+				j0 = j1;	r0 = r1;
+				j1 = j2;	r1 = r2;
+				j2 = j0;	r2 = r0;
+			}
+			// Notify in the proper channel
+			message.client.channels.cache
+				.get("785260212147191888")
+				.send("🎮 **Nouveau résultat**\n"+j1+" ("+r1+")  -  "+j2+" ("+r2+")");
+		}
 	}
 });
 
